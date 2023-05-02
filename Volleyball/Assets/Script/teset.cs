bd7c3bdf32c73a3b0580a8da8075ccd3b0ae4969
mark81806 <72798415+mark81806@users.noreply.github.com>
2023-05-02 23:30:59 +0800

    first commit
    barely started charactor move

---
+++ Volleyball/Assets/Script/teset.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teset : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("aaaa");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
